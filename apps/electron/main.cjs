32c4ad4785acd4d7f4e5b11bf982aa32d611de7b
Matthias Feist <matf.de@gmail.com>
2025-08-16 11:32:32 +0200

    optimized electron.js config

--- apps/electron/main.cjs
+++ apps/electron/main.cjs
@@ -2,19 +2,8 @@
 
 const electron = require('electron');
 
-// Config
-let config = process.env.config ? JSON.parse(process.env.config) : {};
 // Module to control application life.
 const app = electron.app;
-
-/*
- * Per default electron is started with --disable-gpu flag, if you want the gpu enabled,
- * you must set the env var ELECTRON_ENABLE_GPU=1 on startup.
- * See https://www.electronjs.org/docs/latest/tutorial/offscreen-rendering for more info.
- */
-if (process.env.ELECTRON_ENABLE_GPU !== '1') {
-  app.disableHardwareAcceleration();
-}
 
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
@@ -33,16 +22,10 @@ function createWindow() {
    * see https://www.electronjs.org/docs/latest/api/screen
    * Create a window that fills the screen's available work area.
    */
-  let electronSize = electron.screen.getPrimaryDisplay().workAreaSize;
+  const electronSize = electron.screen.getPrimaryDisplay().workAreaSize;
+  app.commandLine.appendSwitch(['autoplay-policy', 'no-user-gesture-required']);
 
-  let electronSwitchesDefaults = [
-    'autoplay-policy',
-    'no-user-gesture-required',
-  ];
-  app.commandLine.appendSwitch(
-    ...new Set(electronSwitchesDefaults, config.electronSwitches),
-  );
-  let electronOptionsDefaults = {
+  const electronOptions = {
     width: electronSize.width,
     height: electronSize.height,
     x: 0,
@@ -51,52 +34,25 @@ function createWindow() {
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: false,
-      zoomFactor: config.zoom,
+      zoomFactor: 1,
     },
     backgroundColor: '#000000',
+    show: false,
+    frame: false,
+    transparent: true,
+    hasShadow: false,
+    fullscreen: true,
   };
 
-  /*
-   * DEPRECATED: "kioskmode" backwards compatibility, to be removed
-   * settings these options directly instead provides cleaner interface
-   */
-  if (config.kioskmode) {
-    electronOptionsDefaults.kiosk = true;
-  } else {
-    electronOptionsDefaults.show = false;
-    electronOptionsDefaults.frame = false;
-    electronOptionsDefaults.transparent = true;
-    electronOptionsDefaults.hasShadow = false;
-    electronOptionsDefaults.fullscreen = true;
-  }
-
-  const electronOptions = Object.assign(
-    {},
-    electronOptionsDefaults,
-    config.electronOptions,
-  );
-
-  // Create the browser window.
   mainWindow = new BrowserWindow(electronOptions);
-
-  /*
-   * and load the index.html of the app.
-   * If config.address is not defined or is an empty string (listening on all interfaces), connect to localhost
-   */
-
   mainWindow.loadURL(`http://localhost:8080/`);
 
   // Open the DevTools if run with "node --run start:dev"
   if (process.argv.includes('dev')) {
-    if (process.env.JEST_WORKER_ID !== undefined) {
-      // if we are running with jest
-      const devtools = new BrowserWindow(electronOptions);
-      mainWindow.webContents.setDevToolsWebContents(devtools.webContents);
-    }
     mainWindow.webContents.openDevTools();
   }
 
-  // simulate mouse move to hide black cursor on start
+  // simulate mouse move to hide cursor on start
   mainWindow.webContents.on('dom-ready', (event) => {
     mainWindow.webContents.sendInputEvent({ type: 'mouseMove', x: 0, y: 0 });
   });
@@ -106,71 +62,10 @@ function createWindow() {
     mainWindow = null;
   });
 
-  if (config.kioskmode) {
-    mainWindow.on('blur', function () {
-      mainWindow.focus();
-    });
-
-    mainWindow.on('leave-full-screen', function () {
-      mainWindow.setFullScreen(true);
-    });
-
-    mainWindow.on('resize', function () {
-      setTimeout(function () {
-        mainWindow.reload();
-      }, 1000);
-    });
-  }
-
-  //remove response headers that prevent sites of being embedded into iframes if configured
-  mainWindow.webContents.session.webRequest.onHeadersReceived(
-    (details, callback) => {
-      let curHeaders = details.responseHeaders;
-      if (config.ignoreXOriginHeader || false) {
-        curHeaders = Object.fromEntries(
-          Object.entries(curHeaders).filter(
-            (header) => !/x-frame-options/i.test(header[0]),
-          ),
-        );
-      }
-
-      if (config.ignoreContentSecurityPolicy || false) {
-        curHeaders = Object.fromEntries(
-          Object.entries(curHeaders).filter(
-            (header) => !/content-security-policy/i.test(header[0]),
-          ),
-        );
-      }
-
-      callback({ responseHeaders: curHeaders });
-    },
-  );
-
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
   });
 }
-
-app.on('activate', function () {
-  /*
-   * On OS X it's common to re-create a window in the app when the
-   * dock icon is clicked and there are no other windows open.
-   */
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-/**
- * Handle errors from self-signed certificates
- */
-app.on(
-  'certificate-error',
-  (event, webContents, url, error, certificate, callback) => {
-    event.preventDefault();
-    callback(true);
-  },
-);
 
 app.whenReady().then(() => {
   createWindow();

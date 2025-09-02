69933822328d57da6fa8c00ef3c8741fccc8fb6b
Matthias Feist <matf.de@gmail.com>
2025-09-02 18:57:32 +0200

    minor style adjustments to the deviation

--- apps/web/src/components/deviations.tsx
+++ apps/web/src/components/deviations.tsx
@@ -12,10 +12,8 @@ export function DeviationsList({ deviations }: { deviations: Deviation[] }) {
 }
 function DeviationItem({ deviation }: { deviation: Deviation }) {
   return (
-    <div className="flex-1 tracking-wide grow min-w-80 line-clamp-2 text-ellipsis overflow-hidden bg-yellow-400/10 border-yellow-400/30 border-3 px-2 py-1">
-      <span className="font-bold text-yellow-500 inline-block pr-3">
-        {deviation.header}
-      </span>
+    <div className="flex-1 tracking-wide grow text-yellow-500 min-w-80 line-clamp-2 text-ellipsis overflow-hidden bg-yellow-400/10 border-yellow-400/30 border-3 px-2 py-1">
+      <span className="font-bold pr-3">{deviation.header}</span>
       <span className="text-yellow-500/70">{deviation.details}</span>
     </div>
   );
